729a32b1fe47f65881663bff86127163b9ca3174
Ian McLoughlin <mcloughlin.ian@gmail.com>
2015-03-13 17:09:10 +0000

    Added wedge

--- wedge.py
+++ wedge.py
@@ -6,12 +6,9 @@ import mpl_toolkits.mplot3d.art3d as pl3d
 fig = pl.figure()
 ax = fig.gca(projection='3d')
 
-x = np.arange(-np.pi, np.pi, 0.01)
-y = np.sin(5.0 * x)
-z = np.zeros(x.size)
-
-pl.hold(True)
-ax.plot(x, y, z)
+x = [0,0,1]
+y = [0,1,1]
+z = [0,1,1]
 
 poly = zip(x, y, z)
 coll = pl3d.Poly3DCollection([poly])
